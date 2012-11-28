e8952240732e0f35b4259fa7710aaaba2e12ee21
Rajesh <rajbasava@gmail.com>
2012-11-28 21:58:08 +0530

    fixing exp/imp issues

---
+++ Registration/src/main/java/com/yvphk/model/form/ParticipantCriteria.java
@@ -0,0 +1,65 @@
+/*
+    Copyright (c) 2012-2015 Yoga Vidya Pranic Healing Foundation of Karnataka.
+    All rights reserved. Patents pending.
+*/
+
+package com.yvphk.model.form;
+
+public class ParticipantCriteria
+{
+    private String name;
+    private String mobile;
+    private String email;
+    private String level;
+    private String foundation;
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public void setName (String name)
+    {
+        this.name = name;
+    }
+
+    public String getMobile ()
+    {
+        return mobile;
+    }
+
+    public void setMobile (String mobile)
+    {
+        this.mobile = mobile;
+    }
+
+    public String getEmail ()
+    {
+        return email;
+    }
+
+    public void setEmail (String email)
+    {
+        this.email = email;
+    }
+
+    public String getLevel ()
+    {
+        return level;
+    }
+
+    public void setLevel (String level)
+    {
+        this.level = level;
+    }
+
+    public String getFoundation ()
+    {
+        return foundation;
+    }
+
+    public void setFoundation (String foundation)
+    {
+        this.foundation = foundation;
+    }
+}
